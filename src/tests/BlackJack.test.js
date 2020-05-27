b09ed1af2cefb899cfe094b761a00202c2e5f522
Amilcar Javier <you@example.com>
2020-05-27 19:50:13 -0400

    todo for tests

--- src/tests/BlackJack.test.js
+++ src/tests/BlackJack.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import BlackJack from '../containers/BlackJack/BlackJack';
 
+//TODO: add appropriate tests
 test('renders learn react link', () => {
   const { getByText } = render(<BlackJack />);
   const linkElement = getByText(/test/i);
