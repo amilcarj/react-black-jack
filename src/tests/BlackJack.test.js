a97feab843f9cdd62cc8110737f8e40983658c80
Amilcar Javier <you@example.com>
2020-05-27 15:20:15 -0400

    set up app structure

--- src/tests/BlackJack.test.js
+++ src/tests/BlackJack.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import BlackJack from '../containers/BlackJack';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const { getByText } = render(<BlackJack />);
+  const linkElement = getByText(/test/i);
   expect(linkElement).toBeInTheDocument();
 });
